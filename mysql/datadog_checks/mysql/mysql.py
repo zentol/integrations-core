1f895bd46e585207f8cc18e00fb01aa68de6329a
Justin Iso <justin.james.iso@gmail.com>
2021-08-13 17:53:59 -0400

    Check maria before timeout

--- mysql/datadog_checks/mysql/mysql.py
+++ mysql/datadog_checks/mysql/mysql.py
@@ -119,13 +119,14 @@ class MySql(AgentCheck):
                 self.version = get_version(db)
                 self._send_metadata()
 
+                self.is_mariadb = self.version.flavor == "MariaDB"
+
                 # set a query timeout; this session variable is only supported on versions higher than 5.7:
                 # https://dev.mysql.com/doc/refman/5.7/en/server-system-variables.html#sysvar_max_execution_time
-                if self.version.version_compatible((5, 7, 0)):
+                if self.version.version_compatible((5, 7, 0)) and not self.is_mariadb:
                     with closing(db.cursor()) as cursor:
                         cursor.execute('SET @@SESSION.max_execution_time = %s', self._config.query_timeout)
 
-                self.is_mariadb = self.version.flavor == "MariaDB"
                 if self._get_is_aurora(db):
                     tags = tags + self._get_runtime_aurora_tags(db)
 
