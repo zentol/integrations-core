d3e92ba9af62c544354208f5b115e516aef7c854
ChristineTChen <christine.chen@datadoghq.com>
2021-08-31 14:10:43 -0400

    Check for nonetype

--- sqlserver/datadog_checks/sqlserver/metrics.py
+++ sqlserver/datadog_checks/sqlserver/metrics.py
@@ -435,6 +435,8 @@ class SqlMasterDatabaseFileStats(BaseSqlServerMetric):
 
         for row in rows:
             column_val = row[value_column_index]
+            if not column_val:
+                continue
             if self.column in ('size', 'max_size'):
                 column_val *= 8  # size reported in 8 KB pages
 
