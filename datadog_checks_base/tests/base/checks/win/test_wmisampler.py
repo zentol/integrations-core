916518c3e1675b56804162b37aa931b3462895ba
Paul Coignet <paul.coignet@datadoghq.com>
2021-09-15 15:35:46 +0200

    Test AND filter

--- datadog_checks_base/tests/base/checks/win/test_wmisampler.py
+++ datadog_checks_base/tests/base/checks/win/test_wmisampler.py
@@ -46,6 +46,14 @@ def test_format_filter_and():
 
 @requires_windows
 @pytest.mark.unit
+def test_format_filter_and2():
+    filters = [(('Name', ('!=', '_total')), ('Name', ('!=', 'model')))]
+    sampler = WMISampler(logger=None, class_name='MyClass', property_names='my.prop', filters=filters)
+    formatted_filters = sampler.formatted_filters
+    assert formatted_filters == " WHERE ( a < '3' )"
+
+@requires_windows
+@pytest.mark.unit
 def test_format_filter_list_expected():
     filters = [{'a': ['<', 3]}]
     sampler = WMISampler(logger=None, class_name='MyClass', property_names='my.prop', filters=filters)
