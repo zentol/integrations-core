3531f0714e9f2f8d76fef5b038a8070118922476
Paul Coignet <paul.coignet@datadoghq.com>
2021-09-27 15:07:05 +0200

    Fix permissions

--- .azure-pipelines/scripts/sqlserver/linux/55_install_odbc.sh
+++ .azure-pipelines/scripts/sqlserver/linux/55_install_odbc.sh
@@ -5,6 +5,6 @@ set -ex
 sudo apt-get update
 sudo apt-get install -y tdsodbc unixodbc unixodbc-dev
 echo "tds_version = 8.0" | sudo tee -a /etc/odbc.ini
-echo "tds_version = 8.0" | sudo tee -a /etc/freetds/freetds.conf
+sudo cat /etc/freetds/freetds.conf | sed "s/tds version =.*/tds version = 8.0/g" | sudo tee /etc/freetds/freetds.conf
 
 set +ex
